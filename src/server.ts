4756bcf701db0d1525649654f2038d7790b74863
Caio <caiopadilha@id.uff.br>
2022-11-10 20:15:23 -0300

    feat: cria método count com o prisma, para contar número de bolões

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'], 
+  // para printar as querys executadas no banco.
+})
 
 async function start() {
   const fastify = Fastify({
@@ -6,8 +12,10 @@ async function start() {
     // loggar a aplicação se tiver erros.
   })
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+
+    return { count }
   })
 
   await fastify.listen({ port: 3333 });
