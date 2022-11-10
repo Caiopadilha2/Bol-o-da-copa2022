1d9041c7cedfa510553448f693d847f78fc097b4
Caio <caiopadilha@id.uff.br>
2022-11-10 20:22:52 -0300

    depend. para ser consumida no react e react native

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { PrismaClient } from '@prisma/client'
+import cors from "@fastify/cors";
 
 const prisma = new PrismaClient({
   log: ['query'], 
@@ -12,13 +13,19 @@ async function start() {
     // loggar a aplicação se tiver erros.
   })
 
+  await fastify.register(cors, {
+    origin: true,
+    // qualquer aplicação acessar o back-end
+    // em produção é mais saudável colocar qual é o domínio.
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 start();
