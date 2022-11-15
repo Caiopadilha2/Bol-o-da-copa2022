67931394feda2829632bc94cfe4211a02cf438d2
Caio <caiopadilha@id.uff.br>
2022-11-14 22:25:12 -0300

    install zod and create endpoint post create new pool

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
+import { z } from "zod";
 import { PrismaClient } from '@prisma/client'
 import cors from "@fastify/cors";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ['query'], 
@@ -23,6 +25,26 @@ async function start() {
     const count = await prisma.pool.count()
 
     return { count }
+  })
+
+  fastify.post('/pools', async (req, res) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } =createPoolBody.parse(req.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code,
+      }
+    })
+
+    return res.status(201).send({code});
   })
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' });
